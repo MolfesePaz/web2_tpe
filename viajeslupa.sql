dacd2061e2fd431e1335ff1d991611031717f61c
MolfesePaz <pazchumolfese2006@gmail.com>
2024-09-17 13:08:56 -0300

    Add files via upload
    Se adjunta archivo .sql en el que se genera la base de datos, las tablas y se efectua un poblado.

---
+++ viajeslupa.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-09-2024 a las 17:53:32
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `viajeslupa`
+--
+CREATE DATABASE viajeslupa
+USE viajeslupa;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresa`
+--
+
+CREATE TABLE `empresa` (
+  `id_empresa` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `ubicacion` varchar(140) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `empresa`
+--
+
+INSERT INTO `empresa` (`id_empresa`, `nombre`, `fecha_inicio`, `ubicacion`) VALUES
+(1, 'ZenAir', '2015-06-23', 'Buenos Aires, Argentina');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `viajes`
+--
+
+CREATE TABLE `viajes` (
+  `id_viaje` int(11) NOT NULL,
+  `origen` varchar(60) NOT NULL,
+  `destino` varchar(50) NOT NULL,
+  `Fecha de salida` date NOT NULL,
+  `Fecha de llegada` date NOT NULL,
+  `id_empresa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `viajes`
+--
+
+INSERT INTO `viajes` (`id_viaje`, `origen`, `destino`, `Fecha de salida`, `Fecha de llegada`, `id_empresa`) VALUES
+(2, 'Buenos Aires, Ezeiza', 'Brasil, Brasilia', '2025-02-05', '2025-02-15', 1),
+(3, 'Buenos Aires, Ezeiza', 'Italia, Roma', '2024-12-18', '2024-12-26', 1),
+(4, 'Buenos Aires, Ezeiza', 'España, Madrid', '2025-07-16', '2025-07-30', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`id_empresa`);
+
+--
+-- Indices de la tabla `viajes`
+--
+ALTER TABLE `viajes`
+  ADD PRIMARY KEY (`id_viaje`),
+  ADD UNIQUE KEY `id_viaje` (`id_viaje`,`id_empresa`),
+  ADD KEY `id_empresa` (`id_empresa`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `viajes`
+--
+ALTER TABLE `viajes`
+  MODIFY `id_viaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `viajes`
+--
+ALTER TABLE `viajes`
+  ADD CONSTRAINT `viajes_ibfk_1` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id_empresa`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
